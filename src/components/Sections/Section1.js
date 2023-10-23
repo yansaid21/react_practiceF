b1249f95bcfa2e9f345e2a50d3a395f6db625feb
jeanSaid <yansaid21@gmail.com>
2023-10-23 00:07:36 -0500

    modales arreglados

--- src/components/Sections/Section1.js
+++ src/components/Sections/Section1.js
@@ -3,34 +3,20 @@ import "./Section1.scss"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import F1 from "../../assets/images/jpg/face1.jpg";
-import F2 from "../../assets/images/jpg/face2.png";
-import F3 from "../../assets/images/jpg/face3.jpg";
 import {images} from "../../assets/images"
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Fab } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 500,
-  heigt: 'auto',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 export const Section1 = ({id}) => {
 
-    const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    
 
     const news= [ {
         _id: "1",
@@ -66,6 +52,18 @@ export const Section1 = ({id}) => {
       },
 
     ] 
+    const [open, setOpen] = React.useState(false);
+    const [selectedNew, setSelectedNew] = React.useState(null);
+
+    const handleOpen = (newId) => {
+      console.log("serviceId",newId);
+      console.log("News", news);
+      const NewNew = news.find((theNew) => theNew._id === newId);
+      console.log("service", NewNew);
+      setSelectedNew(NewNew);
+      setOpen(true);
+    };
+  const handleClose = () => setOpen(false);
     const sliderSettings = {
         dots: true,
         infinite: true,
@@ -74,72 +72,58 @@ export const Section1 = ({id}) => {
         slidesToScroll: 1,
       };
   return (
-    <div className='Section1'>Section1
     <div className="section-content" id={id}>
-    {news.length >0?(
-            news.map((theNew) =>{
-    <Slider {...sliderSettings}>
-                <div key={theNew._id}>
-                    <img src={theNew.avatar}  />
-                    <p className='sliderTitle'>{theNew.title}</p>
-                    <p className='sliderSubtitle'>{theNew.subtitle}</p>
-                    <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-                </div>
-        </Slider>
-            }
-        )):(<p>no hay noticias</p>)}
-    <Slider {...sliderSettings}>
-        
-          <div >
-          <img src={F1}  />
-          <p className='sliderTitle'>facebook crece</p>
-          <p className='sliderSubtitle'>crecimiento exponencial</p>
-          <div className='sliderCont'>
+      <div className='Section1'>
+    {news.length > 0 ? (
+  <Slider className='SectionSlider'{...sliderSettings}>
+    {news.map((theNew) => (
+      <div key={theNew._id}>
+        <img src={theNew.avatar}/>
+        <p className='sliderTitle'>{theNew.title}</p>
+        <p className='sliderSubtitle'>{theNew.subtitle}</p>
+        <div className='sliderCont'>
 
-          <Button className='sliderButton' variant="contained" onClick={handleOpen}>Ver más</Button>
-      <Modal
+        <Button className='sliderButton' variant="contained" onClick={() => handleOpen(theNew._id)}>Ver más</Button>
+
+      {selectedNew &&(
+      <Modal className='Modal-new'
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
         >
-        <Box sx={style}>
-            <img src={images.facebook3} />
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-          facebook crece
+        <Box className='Box-New'>
+          <div className='Img-box-container'>
+            <img  src={selectedNew.avatar} />
+          </div>
+          <Typography id="modal-modal-title" variant="h4" component="h6" color='#009d9d'>
+          {selectedNew.title}
           </Typography>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-          crecimiento exponencial
+          {selectedNew.subtitle}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Facebook adquiere una suma gigante de acciones gracias a sus nuevas implementaciones
+          <Typography id="modal-modal-description">
+            <p></p>
+          {selectedNew.description}
           </Typography>
+          <div className="button-fav-group">
+                          <Fab color="secondary" aria-label="Favorite icon">
+                            <FavoriteIcon />
+                          </Fab>
+                          <Fab color="primary" aria-label="Favorite icon">
+                            <AddShoppingCartIcon />
+                          </Fab>
+                          </div>
         </Box>
       </Modal>
+      )}
           </div>
-          </div>
-          <div>
-          <img src={F2} />
-          </div>
-          <div>
-          <img src={F3} />
-          </div>
-        </Slider>
+      </div>
+    ))}
+  </Slider>
+) : (
+  <p>no hay noticias</p>
+)}
     </div>
     </div>
   )
--- src/components/Sections/Section3.js
+++ src/components/Sections/Section3.js
@@ -3,7 +3,7 @@ import "./Section3.scss"
 
 export const Section3 = ({id}) => {
   return (
-    <div className='Section3'>Section3
+    <div className='Section3'>
     <div className="section-content" id={id}></div>
     </div>
   )
